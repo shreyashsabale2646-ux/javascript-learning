33309874d0faead674bfbf17049d86e7aab6fd3f
Shreyash Sabale <shreyashsabale2646@gmail.com>
2026-09-25 19:55:30 +0530

    scope level and hoisting

--- 03-basics/02_scope.js
+++ 03-basics/02_scope.js
@@ -10,3 +10,38 @@ let d =1000   //  GLOBAL SCOPE
  //console.log(a);
  //console.log(b);
  console.log(c);  // it came out of the scope 
+
+
+
+ function one(){
+    const username = "hitesh"
+
+    function two(){
+      const website = "yt"
+      console.log(username);
+    }
+
+    // console.log(website);  // accessing the website out of scope 
+
+    two()
+ }
+
+ one()
+
+ // +++++++++++++++++++++++++++++++ intresting +++++++++++++++++++++++++++++++++
+
+
+ addOne(5)   // you can define first 
+ function addOne(num){
+  return num+1
+ }
+
+
+
+ // addTwo(5)   error  hoisting 
+
+ const addTwo = function(num){      // called as expression to 
+  return num +2
+ }
+
+ addTwo(5) 
